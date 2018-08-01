d861112b8a807eec8a36ff75b2cd654f5e839d65
Rahul Kandu <rkandu@openjdk.org>
2018-08-01 06:43:33 -0700

    SVML Linux .s files and Windows build fix

--- src/hotspot/share/utilities/globalDefinitions_vecApi.hpp
+++ src/hotspot/share/utilities/globalDefinitions_vecApi.hpp
@@ -30,7 +30,7 @@
 #define SHARE_VM_UTILITIES_GLOBALDEFINITIONS_VECAPI_HPP
 
 // VS2013 required to build .s files for math intrinsics
-#if defined(_WIN64) && (defined(_MSC_VER) && (_MSC_VER >= 1800))
+#if defined(_WIN64) && (defined(_MSC_VER) && (_MSC_VER >= 2000))
 #define __VECTOR_API_MATH_INTRINSICS_COMMON
 #define __VECTOR_API_MATH_INTRINSICS_WINDOWS
 #endif
