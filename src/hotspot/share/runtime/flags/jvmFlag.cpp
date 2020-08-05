openjdk/jdk

8235908: omit ThreadPriorityPolicy warning when value is set from image
Reviewed-by: dholmes, clanger, kvn

src/hotspot/share/runtime/flags/jvmFlag.cpp
@@ -153,6 +153,10 @@ bool JVMFlag::is_command_line() {
   return (_flags & ORIG_COMMAND_LINE) != 0;
 }
 
+bool JVMFlag::is_jimage_resource() {
+  return (get_origin() == JIMAGE_RESOURCE);
+}
+
 void JVMFlag::set_command_line() {
   _flags = Flags(_flags | ORIG_COMMAND_LINE);
 }
@@ -837,6 +841,12 @@ bool JVMFlagEx::is_ergo(JVMFlagsEnum flag) {
 
 bool JVMFlagEx::is_cmdline(JVMFlagsEnum flag) {
   return flag_from_enum(flag)->is_command_line();
+}
+
+bool JVMFlagEx::is_jimage_resource(JVMFlagsEnum flag) {
+  assert((size_t)flag < JVMFlag::numFlags, "bad command line flag index");
+  JVMFlag* f = &JVMFlag::flags[flag];
+  return f->is_jimage_resource();
 }
 
 void JVMFlagEx::setOnCmdLine(JVMFlagsEnum flag) {

src/hotspot/share/runtime/globals_extension.hpp
@@ -88,6 +88,7 @@ class JVMFlagEx : JVMFlag {
   static bool is_default(JVMFlagsEnum flag);
   static bool is_ergo(JVMFlagsEnum flag);
   static bool is_cmdline(JVMFlagsEnum flag);
+  static bool is_jimage_resource(JVMFlagsEnum flag);
 
   static void setOnCmdLine(JVMFlagsEnum flag);
 
@@ -132,6 +133,7 @@ ALL_FLAGS(FLAG_MEMBER_SET_DEVELOP,
 #define FLAG_IS_DEFAULT(name)         (JVMFlagEx::is_default(FLAG_MEMBER_ENUM(name)))
 #define FLAG_IS_ERGO(name)            (JVMFlagEx::is_ergo(FLAG_MEMBER_ENUM(name)))
 #define FLAG_IS_CMDLINE(name)         (JVMFlagEx::is_cmdline(FLAG_MEMBER_ENUM(name)))
+#define FLAG_IS_JIMAGE_RESOURCE(name) (JVMFlagEx::is_jimage_resource(FLAG_MEMBER_ENUM(name)))
 
 #define FLAG_SET_DEFAULT(name, value) ((name) = (value))
 

Tencent/TencentKona-11

src/hotspot/share/runtime/flags/jvmFlag.cpp
@@ -294,6 +294,10 @@ bool JVMFlag::is_command_line() {
   return (_flags & ORIG_COMMAND_LINE) != 0;
 }
 
+bool JVMFlag::is_jimage_resource() {
+  return (get_origin() == JIMAGE_RESOURCE);
+}
+
 void JVMFlag::set_command_line() {
   _flags = Flags(_flags | ORIG_COMMAND_LINE);
 }
@@ -987,6 +991,12 @@ bool JVMFlag::wasSetOnCmdline(const char* name, bool* value) {
   if (result == NULL) return false;
   *value = result->is_command_line();
   return true;
+}
+
+bool JVMFlagEx::is_jimage_resource(JVMFlags flag) {
+  assert((size_t)flag < JVMFlag::numFlags, "bad command line flag index");
+  JVMFlag* f = &JVMFlag::flags[flag];
+  return f->is_jimage_resource();
 }
 
 void JVMFlagEx::setOnCmdLine(JVMFlagsWithType flag) {

src/hotspot/share/runtime/globals_extension.hpp
@@ -299,6 +299,7 @@ typedef enum {
 #define FLAG_IS_DEFAULT(name)         (JVMFlagEx::is_default(FLAG_MEMBER(name)))
 #define FLAG_IS_ERGO(name)            (JVMFlagEx::is_ergo(FLAG_MEMBER(name)))
 #define FLAG_IS_CMDLINE(name)         (JVMFlagEx::is_cmdline(FLAG_MEMBER(name)))
+#define FLAG_IS_JIMAGE_RESOURCE(name) (JVMFlagEx::is_jimage_resource(FLAG_MEMBER(name)))
 
 #define FLAG_SET_DEFAULT(name, value) ((name) = (value))
 
@@ -330,6 +331,7 @@ class JVMFlagEx : JVMFlag {
   static bool is_default(JVMFlags flag);
   static bool is_ergo(JVMFlags flag);
   static bool is_cmdline(JVMFlags flag);
+  static bool is_jimage_resource(JVMFlags flag);
 
   static void setOnCmdLine(JVMFlagsWithType flag);
 };